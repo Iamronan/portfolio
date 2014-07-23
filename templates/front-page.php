b5b521ee68c1eecb702771b3f5a278eb371ff83f
Ronan Vallelly <ronan@traffic-design.co.uk>
2014-07-23 17:34:15 +0100

    landing page
    Signed-off-by: Ronan Vallelly <ronan@traffic-design.co.uk>

---
+++ templates/front-page.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ Template Name: Home Page
+ *
+ * This is your custom page template. You can create as many of these as you need.
+ * Simply name is "page-whatever.php" and in add the "Template Name" title at the
+ * top, the same way it is here.
+ *
+ * When you create your page, you can just select the template and viola, you have
+ * a custom page template to call your very own. Your mother would be so proud.
+ *
+ * For more info: http://codex.wordpress.org/Page_Templates
+*/
+?>
+
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="cf">
+
+<ul class="m-all t-all l-all">
+
+	<li class="m-all t-1of3 d-1of3"><img src="http://localhost/iamronan_redesign/wordpress/wp-content/uploads/2014/07/stir_8_7.jpg"> </li>
+    <li class="m-all t-1of3 d-1of3"><img src="http://localhost/iamronan_redesign/wordpress/wp-content/uploads/2014/07/stir_8_7.jpg"></li>
+    <li class="m-all t-1of3 d-1of3"><img src="http://localhost/iamronan_redesign/wordpress/wp-content/uploads/2014/07/stir_8_7.jpg"></li>
+    <li class="m-all t-1of3 d-1of3"><img src="http://localhost/iamronan_redesign/wordpress/wp-content/uploads/2014/07/stir_8_7.jpg"></li>
+    <li class="m-all t-1of3 d-1of3"><img src="http://localhost/iamronan_redesign/wordpress/wp-content/uploads/2014/07/stir_8_7.jpg"></li>
+    <li class="m-all t-1of3 d-1of3"><img src="http://localhost/iamronan_redesign/wordpress/wp-content/uploads/2014/07/stir_8_7.jpg"></li>
+
+</ul>    
+
+
+						</div>
+
+
+				</div>
+
+			</div>
+
+
+<?php get_footer(); ?>
