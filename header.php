bda1675a5777d371f16dd5cdddc2fd08723ecda1
Ronan Vallelly <ronan@traffic-design.co.uk>
2014-07-25 15:31:44 +0100

    home page updates
    Signed-off-by: Ronan Vallelly <ronan@traffic-design.co.uk>

--- header.php
+++ header.php
@@ -92,7 +92,7 @@
 					</svg>
 				</div>
 				<p class="intro-text">
-				My name is Ronan Vallelly,  a visual designer and front end
+				My name is <span> Ronan Vallelly </span>,  a visual designer and front end
 				developer currently living in Glasgow. I love to code, design stuff and occasionally spin records. I craft beautiful, functional and simple experiences for web and mobile
 				</p>
 
